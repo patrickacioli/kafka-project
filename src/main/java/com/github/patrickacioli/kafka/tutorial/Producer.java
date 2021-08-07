0560b3cafb273f92a56cb6ba13a47f62ed3237c6
Patrick Acioli <patrick@acioli.dev>
2021-08-07 17:48:34 -0300

    feat: add first producer example

--- src/main/java/com/github/patrickacioli/kafka/tutorial/Producer.java
+++ src/main/java/com/github/patrickacioli/kafka/tutorial/Producer.java
@@ -1,9 +1,39 @@
 package com.github.patrickacioli.kafka.tutorial;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class Producer {
 
     public static void main(String[] args) {
-        System.out.println("Testing!!");
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // Producer Config
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // Create a Record
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello world");
+
+        // Send data
+        producer.send(record);
+
+        // Flush data
+        // If you trying run without call this methods, data never sended to broker
+        // because .send() is async and program finished first that this methid.
+        producer.flush();
+        producer.close();
+
     }
 
 }
